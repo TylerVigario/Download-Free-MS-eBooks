34cc1d518cb2cc3125b9524af7cc5667d6025447
Tyler Vigario <TylerVigario90@gmail.com>
2017-07-17 22:12:30 -0700

    First Commit

---
+++ Download Free MS eBooks/MainForm.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace Download_Free_MS_eBooks
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.objectListView1 = new BrightIdeasSoftware.ObjectListView();
+            this.olvColumn2 = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            this.olvColumn1 = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            this.olvColumn3 = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            this.button1 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.objectListView1)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // objectListView1
+            // 
+            this.objectListView1.AllColumns.Add(this.olvColumn2);
+            this.objectListView1.AllColumns.Add(this.olvColumn1);
+            this.objectListView1.AllColumns.Add(this.olvColumn3);
+            this.objectListView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.objectListView1.CellEditUseWholeCell = false;
+            this.objectListView1.CheckBoxes = true;
+            this.objectListView1.CheckedAspectName = "Active";
+            this.objectListView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.olvColumn2,
+            this.olvColumn1,
+            this.olvColumn3});
+            this.objectListView1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.objectListView1.FullRowSelect = true;
+            this.objectListView1.Location = new System.Drawing.Point(12, 12);
+            this.objectListView1.Name = "objectListView1";
+            this.objectListView1.ShowGroups = false;
+            this.objectListView1.Size = new System.Drawing.Size(727, 311);
+            this.objectListView1.TabIndex = 0;
+            this.objectListView1.UseCompatibleStateImageBehavior = false;
+            this.objectListView1.View = System.Windows.Forms.View.Details;
+            // 
+            // olvColumn2
+            // 
+            this.olvColumn2.AspectName = "Active";
+            this.olvColumn2.CheckBoxes = true;
+            this.olvColumn2.Text = "Active";
+            this.olvColumn2.Width = 48;
+            // 
+            // olvColumn1
+            // 
+            this.olvColumn1.AspectName = "URL";
+            this.olvColumn1.Text = "URL";
+            this.olvColumn1.Width = 190;
+            // 
+            // olvColumn3
+            // 
+            this.olvColumn3.AspectName = "Name";
+            this.olvColumn3.FillsFreeSpace = true;
+            this.olvColumn3.Text = "Name";
+            this.olvColumn3.Width = 306;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(664, 329);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Download";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripProgressBar1,
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 360);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(751, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 16);
+            this.toolStripProgressBar1.Step = 1;
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(57, 17);
+            this.toolStripStatusLabel1.Text = "Waiting...";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(751, 382);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.objectListView1);
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.Text = "We all love learning!";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.objectListView1)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private BrightIdeasSoftware.ObjectListView objectListView1;
+        private BrightIdeasSoftware.OLVColumn olvColumn1;
+        private BrightIdeasSoftware.OLVColumn olvColumn2;
+        private System.Windows.Forms.Button button1;
+        private BrightIdeasSoftware.OLVColumn olvColumn3;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
+
---
+++ Download Free MS eBooks/MainForm.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows.Forms;
+using System.Net;
+
+namespace Download_Free_MS_eBooks
+{
+    public partial class MainForm : Form
+    {
+        private List<Download> urls;
+        private List<Download> downloads;
+        private WebClient wClient;
+        private string downloadLocation;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            //
+            this.olvColumn1.AspectToStringConverter = delegate(object x) {
+                Uri url = (Uri)x;
+                return url.ToString();
+            };
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            wClient = new WebClient();
+            //
+            urls = downloadBookList();
+            //
+            objectListView1.SetObjects(urls);
+        }
+
+        private List<Download> downloadBookList()
+        {
+            string temp;
+            //
+            try
+            {
+                temp = wClient.DownloadString("https://drive.google.com/uc?export=download&id=0B-ULAF28Y63jY00wZEtWSmRTUWM");
+            }
+            catch (WebException)
+            {
+                MessageBox.Show("File list URL is down. Look for update from developer of this software.");
+                return null;
+            }
+            //
+            string[] lines = temp.Split(new string[] { "\r\n", "\t\t" }, StringSplitOptions.None);
+            List<Download> urls = new List<Download>();
+            //
+            foreach (string line in lines)
+            {
+                if (!String.IsNullOrEmpty(line) && line.Trim() != "MSFT eBooks")
+                {
+                    try
+                    {
+                        string[] parts = line.Split(',');
+                        urls.Add(new Download(new Uri(parts[0].Trim()), parts[1].Trim()));
+                    }
+                    catch(UriFormatException ex)
+                    {
+                        // Send error maybe
+                        // Log it for sure
+                    }
+                }
+            }
+            //
+            return urls;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() != DialogResult.OK) { return; }
+            downloadLocation = folderBrowserDialog1.SelectedPath;
+            downloads = new List<Download>();
+            //
+            foreach (Download details in objectListView1.Objects)
+            {
+                if (details.Active)
+                {
+                    downloads.Add(details);
+                }
+            }
+            //
+            toolStripProgressBar1.Maximum = downloads.Count;
+            //
+            wClient.DownloadFileCompleted += WClient_DownloadFileCompleted;
+            toolStripStatusLabel1.Text = "Downloading " + downloads[0].Name;
+            wClient.DownloadFileAsync(downloads[0].URL, downloadLocation + "\\" + downloads[0].Name);
+            downloads.Remove(downloads[0]);
+        }
+
+        private void WClient_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            toolStripStatusLabel1.Text = "Downloading " + downloads[0].Name;
+            wClient.DownloadFileAsync(downloads[0].URL, downloadLocation + "\\" + downloads[0].Name);
+            downloads.Remove(downloads[0]);
+            toolStripProgressBar1.PerformStep();
+        }
+    }
+
+    public class Download
+    {
+        private Uri _uri;
+        private bool _active = true;
+        private string _name = "";
+
+        public Download(Uri u, string n)
+        {
+            _uri = u;
+            _name = n;
+        }
+
+        public Uri URL
+        {
+            get { return _uri; }
+        }
+
+        public bool Active
+        {
+            get { return _active; }
+            set { _active = value; }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+        }
+    }
+}
